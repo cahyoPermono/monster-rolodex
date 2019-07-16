6a094db425b2e88c0a63f70e708510290677a993
Cahyo <notfound.adi@gmail.com>
2019-07-16 20:59:21 +0700

    Updates

---
+++ precache-manifest.442aa7164e9a15c162bc3b8727233fa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c003463c2f0b417f1bf1ed38dbfc6b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "218630242f84e674ea37",
+    "url": "/monster-rolodex/static/css/main.c046dd2c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "218630242f84e674ea37",
+    "url": "/monster-rolodex/static/js/main.7bdfaa4e.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
